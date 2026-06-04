24c3e1ffff55db00664963a7a2a9644f044c3a04
amarbut <anna.marbut@gmail.com>
2026-06-04 16:17:29 -0600

    untrack slurm and scratch files

--- VM_pip_installs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pip install git+https://github.com/huggingface/transformers
-
-pip install torch transformers[torch] evaluate datasets scipy scikit-learn
-
-pip install faiss-gpu faiss-cpu Isoscore
